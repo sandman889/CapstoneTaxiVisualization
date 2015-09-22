50cc386844dae6e69c8bb97bbf5386985404753f
sandman889 <tsande14@kent.edu>
2015-09-22 18:11:53 -0400

    More cleaning of file structure, map shows up, ready for further development

--- CapstoneTaxiVisualization/Controllers/HomeController.cs
+++ CapstoneTaxiVisualization/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Microsoft.SqlServer.Types;
+using CapstoneTaxiVisualization.Models;
 
 namespace CapstoneTaxiVisualization.Controllers
 {
@@ -11,6 +14,42 @@ namespace CapstoneTaxiVisualization.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string GetNearestNeighbor(LatLong point)
+        {
+            //run the sql query to get the nearest neighbor within a certain distance, increase the distance by powers of two until one is found
+            var test = "";
+
+            return JsonConvert.SerializeObject(test);
+        }
+
+        [HttpPost]
+        public string GetPointsInPolygonRegion(List<LatLong> boundPoints)
+        {
+            var geoPoly = new SqlGeographyBuilder();
+
+            //set the SRID and chose the sql geography datatype
+            geoPoly.SetSrid(4326);
+            geoPoly.BeginGeography(OpenGisGeographyType.Polygon);
+
+            foreach (var point in boundPoints)
+            {
+                //add each point to the geography poly
+                geoPoly.AddLine(point.Latitude, point.Longitude);
+            }
+
+            //add the first point again because it will complete the polygon
+            geoPoly.AddLine(boundPoints.First().Latitude, boundPoints.First().Longitude);
+
+            //end the configuration of the geography
+            geoPoly.EndFigure();
+            geoPoly.EndGeography();
+
+            SqlGeography sqlPoly = geoPoly.ConstructedGeography;
+
+            //todo, not this, actually call a stored procedure
+            return sqlPoly.ToString();
         }
     }
 }
---
+++ CapstoneTaxiVisualization/Models/LatLong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneTaxiVisualization.Models
+{
+    public class LatLong
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
